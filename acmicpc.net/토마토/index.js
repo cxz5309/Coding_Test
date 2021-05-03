a68ef3568858a218b32b793898c50fec3ddd35b3
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-05-03 23:56:03 +0900

    Update index.js

--- acmicpc.net/토마토/index.js
+++ acmicpc.net/토마토/index.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const stdin = (process.platform === 'linux'
     ? fs.readFileSync('/dev/stdin').toString()
-    : `
-6 4
+    : `6 4
 0 0 0 0 0 0
 0 0 0 0 0 0
 0 0 0 0 0 0
@@ -17,32 +16,33 @@ var horizontal;
 var vertical;
 var lineSplit = [];
 
-lineSplit = stdin[0];
+lineSplit = stdin[0].split(' ');
 vertical = Number(lineSplit[0]);
 horizontal = Number(lineSplit[1]);
-stdin.shift();
+console.log(vertical);
+console.log(horizontal);
 stdin.shift();
 
 var map = Array.from(Array(horizontal), () => new Array(vertical).fill(0));
 
 
-for(let i=0; i<vertical; i++){
-    lineSplit = stdin[0];
-    for(let j = 0;j<horizontal;j++){
-        
+for(let i=0; i<horizontal; i++){
+    lineSplit = stdin[0].split(' ');
+    for(let j = 0;j<vertical;j++){
+        if(Number(lineSplit[j]) == 1){
+            map[i][j] = 1;
+        }
     }
+    stdin.shift();
 }
 
+console.log(map);
 
 //--------------------------------------------------------
-
-let a = wordSplit[0];
-let b = wordSplit[1];
 
 function solution(input1, input2){
    let answer = [];
    return answer;
 }
 
-
-console.log(solution(wordSplit[0], wordSplit[1]).join("\n"));
+//console.log(solution(wordSplit[0], wordSplit[1]).join("\n"));
