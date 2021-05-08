0fee747c33c7853c8bffedb9d5624f1e11b4c637
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-05-08 17:47:00 +0900

    Update index.js

--- acmicpc.net/토마토/index.js
+++ acmicpc.net/토마토/index.js
@@ -1,98 +1,108 @@
 const fs = require('fs');
-const stdin = (process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
-    : `6 4
-0 0 0 0 0 0
-0 0 0 0 0 0
-0 0 0 0 0 0
-0 0 0 0 0 1`
+const stdin = (process.platform === 'linux' ?
+    fs.readFileSync('/dev/stdin').toString() :
+    `5 5
+-1 1 0 0 0
+0 -1 -1 -1 0
+0 -1 -1 -1 0
+0 -1 -1 -1 0
+0 0 0 0 0`
 ).split('\n');
- 
-/*case 't'
-'1 1'
-...*/
 
-var horizontal;
-var vertical;
+const moveX = [0, 1, 0, -1];
+const moveY = [1, 0, -1, 0];
+
+var height;
+var width;
 var lineSplit = [];
 
-lineSplit = stdin[0].split(' ');
-vertical = Number(lineSplit[0]);
-horizontal = Number(lineSplit[1]);
-console.log(vertical);
-console.log(horizontal);
-stdin.shift();
+var shift = 0;
 
-var map = Array.from(Array(horizontal), () => new Array(vertical).fill(0));
-var visited = [];
+lineSplit = stdin[0 + shift].split(' ');
+width = Number(lineSplit[0]);
+height = Number(lineSplit[1]);
+// console.log(width);
+// console.log(height);
+shift++;
 
-for(let i=0; i<horizontal; i++){
-    lineSplit = stdin[0].split(' ');
-    for(let j = 0;j<vertical;j++){
-        if(Number(lineSplit[j]) == 1){
-            map[i][j] = 1;
+var map = Array.from(Array(height), () => new Array(width));
+var visited = Array.from(Array(height), () => new Array(width));
+unVisitQ = [];
+let tmpNum;
+
+for (let i = 0; i < height; i++) {
+    lineSplit = stdin[0 + shift].split(' ');
+    for (let j = 0; j < width; j++) {       
+        tmpNum = Number(lineSplit[j]);    
+
+        map[i][j] = tmpNum;
+        visited[i][j] = tmpNum == -1 ? 1 : 0;
+
+        if (tmpNum >= 1) {
+            unVisitQ.push([j, i, 0]);
         }
     }
-    stdin.shift();
+    shift++;
 }
 
-console.log(map);
-
+//console.log(map);
 //--------------------------------------------------------
 
 //BFS
-function BFS(map, startX, startY){
+function BFS(map) {
     let nextX;
     let nextY;
-    unVisitQ = [];
-    unVisitQ.push([startX, startY, count]);
-
+    let current;
     let retu = 0;
-    while (unVisitQ.length > 0) {
-        const current = unVisitQ.shift();
-        current.count++;
 
-        if(retu<current.count){
-            retu = current.count;
-        }
-        for(var i = 0; i < 4; i++){
+    while (unVisitQ.length > 0) {
+        //망할 js shift 속도가 queue 구현한것보다 325배 느리단다..
+        //그럼 큐를 만들어놓던가..
+        current = unVisitQ.shift();
+        visited[current[1]][current[0]] = 1;
+        // console.log('현재위치 ' + current[0], ', ' + current[1]);
+        // console.log('현재수 ' + current[2]);
+
+        retu = retu < current[2] ? current[2] : retu;
+
+        for (var i = 0; i < 4; i++) {
             nextX = current[0] + moveX[i];
             nextY = current[1] + moveY[i];
+            nextCount = current[2] + 1;
 
             //큐에 삽입 제외 조건
-            if(!range(nextX, nextY))
+            if (!range(nextX, nextY) || map[nextY][nextX] == -1 || visited[nextY][nextX] == 1)
                 continue;
 
-            if(map[nextX][nextY] == -1)
-                continue;
-
-            if(visited[nextX][nextY] == 1)
-                continue;
-            
-            visited[nextX][nextY] = 1;
-            unVisitQ.push([nextX, nextY])
+            visited[nextY][nextX] = 1;
+            unVisitQ.push([nextX, nextY, nextCount])
         }
     }
     return retu;
 }
 
-function range(x, y){
-    if (x<0||y<0||x>=width||y>=height){
+function range(x, y) {
+    if (x < 0 || y < 0 || x >= width || y >= height) {
         return false;
     }
     return true;
 }
 
-function solution(map){
-   let answer;
-   for(let i = 0;i<horizontal;i++){
-       for(let j = 0; j<vertical; j++){
-           if(map[i][j] != -1 && visited[i][j] != 1){
-               BFS(map, i, j);
-           }
-       }
-   }
-   return answer;
+function solution(map) {
+    let answer;
+    let flag = 123456789;
+
+    answer = BFS(map);
+
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if(visited[i][j] != 1){
+                flag = -1;
+            }
+        }
+    }
+    answer = flag < answer ? flag : answer
+    return answer;
 }
 
 console.log(solution(map));
