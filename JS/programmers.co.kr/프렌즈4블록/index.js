ee3566bd507c395bab47cd6b93ace7a5fb853e3f
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-06-15 17:35:43 +0900

    Update index.js

--- JS/programmers.co.kr/프렌즈4블록/index.js
+++ JS/programmers.co.kr/프렌즈4블록/index.js
@@ -10,107 +10,107 @@ CCBBF`
 
 const m = Number(stdin[0].split(' ')[0]);
 const n = Number(stdin[0].split(' ')[1]);
-
 stdin.shift();
-let map = [];
-let chk = [];
 
-const moveCrossX = [0, 1, 0, -1];
-const moveCrossY = [1, 0, -1, 0];
+//--------------------------------------------------------
+
 const moveSquareX = [0, 1, 0, 1];
 const moveSquareY = [0, 0, 1, 1];
 
-for(let i = 0; i < m; i++){
-    map.push(stdin.shift().split(''));
-    chk.push(new Array(n).fill(0));
-}
-console.log(map);
-console.log(chk);
-//--------------------------------------------------------
-
-function chkSquare(x, y){
-    if(x + 1 >= n || y + 1 >= m){
-        return false;
-    }
-
-    const character = map[y][x];
-    for(let i = 0; i<4; i++){
-        if(map[y + moveSquareY[i]][x + moveSquareX[i]] !== character){
-            return false;
-        }
-    }
-    for(let i = 0; i<4; i++){
-        chk[y + moveSquareY[i]][x + moveSquareX[i]] = 1;
-    }
-    return true;
-}
+let map = [];
+let chk = [];
+let M, N;
 
 function range(x, y){
-    if(x<0 || y<0 || x>=n || y>=m){
+    if(x<0 || y<0 || x >= N || y >= M){
         return false;
     }
     return true;
 }
 
-function BFS(x, y){
-    unVisit = [];
-    unVisit.push([x, y]);
+function chkSquare(x, y){
+    const character = map[y][x];
+    if(character === null) return false;
+        
+    let nextX;
+    let nextY;
+    //console.log("xy : " + x + " " + y);
 
-    if(chkSquare(x, y)){
-        chk[y][x] = 2;
+    for(let i = 1; i<4; i++){
+        nextX = x + moveSquareX[i];
+        nextY = y + moveSquareY[i];
+        //console.log("next xy : " + nextX + " " + nextY)
+        if(!range(nextX, nextY)) return false;
+        if(map[nextY][nextX] !== character) return false;
     }
-    else{
-        chk[y][x] = -1;
+
+    for(let i = 0; i<4; i++){
+        nextX = x + moveSquareX[i];
+        nextY = y + moveSquareY[i];
+        chk[nextY][nextX] = 1;
     }
-
-    while(unVisit.length > 0){
-        let tmp = unVisit.shift();
-        let next = [];//next[0] = x좌표, next[1] = y좌표
-
-        if(chkSquare(tmp[0], tmp[1])){
-            console.log("!!!!");
-            chk[tmp[1]][tmp[0]] = 2;
-        }
-        else{
-            chk[tmp[1]][tmp[0]] = -1;
-        }
-
-        console.log(tmp[0] + " " + tmp[1]);
-        for(let i = 0; i<4; i++){
-            next = [tmp[0] + moveCrossY[i], tmp[1] + moveCrossX[i]];
-
-            if(!range(next[0], next[1])){
-                continue;
-            }
-            if(chk[next[1]][next[0]] == -1 || chk[next[1]][next[0]] == 2 ){
-                continue;
-            }
-            unVisit.push([next[0], next[1]]);
-            chk[next[1]][next[0]] = -1;
-        }
-    }
+    return true;
 }
 
-function idiot(){
-    for(let i = 0; i < m; i++){
-        for(let j = 0; j < n; j++){
-            chkSquare(i, j);
-        }
-    }
-}
-
-function solution() {
-    let answer = 0;
-    idiot();
+function change(x, y){
+    if(chk[y][x] !== 1) return 0;
     
-    console.log(map);
-    console.log(chk);
-    for(let i = 0;i<m;i++){
-        answer += chk[i].reduce((cnt, now)=>{
-            return cnt + now;
-        }, 0);
+    for(let j = y - 1; j>=0; j--){
+        map[j + 1][x] = range(x, y - 1) ? map[j][x] : null;
+        map[j][x] = null;
     }
+    
+    chk[y][x] = 0;
+    return 1;
+}
+
+function chain(){
+    let cnt = 0;
+    let isChange = true;
+
+    while(isChange){
+        //console.log("-------------------------------")
+        isChange = false;
+        //찾기
+        for(let i = 0; i < M; i++){
+            for(let j = 0; j < N; j++){
+                if(chkSquare(j, i)){
+                    isChange = true;
+                }
+            }
+        }
+        //console.log(map);
+        //console.log(chk);
+
+        //clear & down & cnt
+        for(let i = 0; i < M; i++){
+            for(let j = 0; j < N; j++){
+                cnt += change(j, i);
+            }
+        }
+        //console.log(map);
+        //console.log(chk);
+        if(!isChange){
+            break;
+        }
+    }
+
+    //최종 결과 계산
+    return cnt;
+}
+
+function solution(m, n, board) {
+    var answer = 0;
+    M = m;
+    N = n;
+    for(let i = 0; i < m; i++){
+        map.push(board[i].split(''));
+    }
+    chk = Array.from(Array(m), () => Array(n).fill(0))
+    
+    answer = chain();
+    
     return answer;
 }
 
-console.log(solution());
+console.log(solution(m, n, stdin));
